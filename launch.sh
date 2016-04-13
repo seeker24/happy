873846e03a5bf6484261e078ef11598b3ef23df8
seeker24 <golsarchat@chmail.ir>
2016-04-13 23:01:02 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+--by @Hextor_ch
+
 #!/usr/bin/env bash
 
 THIS_DIR=$(cd $(dirname $0); pwd)
@@ -110,6 +112,11 @@ else
     echo "Run $0 install"
     exit 1
   fi
-
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+   sleep 3
+  done
 fi
+
+--by @Hextor_ch
